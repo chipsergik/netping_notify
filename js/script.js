405d3607d6967760c825543bcc814776e7b4d512
chipsergik <chipsergik@gmail.com>
2015-11-03 02:38:46 +0300

    notification edit

---
+++ js/script.js
@@ -0,0 +1,25 @@
+﻿$(function () {
+    $('.tab-navs a').on('click', function (e) {
+        e.preventDefault();
+        $('.tab-content > div').removeClass('active');
+        var $tab = $(this);
+        $('.tab-content > div' + $tab.attr('href')).addClass('active');
+        $tab.parent().siblings().removeClass('active')
+            .end().addClass('active');
+    });
+
+    $('.register .btn.btn-primary').on('click', function (e) {
+        e.preventDefault();
+        $('.overlay').show();
+    });
+
+    $('.delete-confirmation .btn.btn-cancel').on('click', function (e) {
+        e.preventDefault();
+        $('.overlay').hide();
+    });
+
+    $('.remove-sensor').on('click', function (e) {
+        e.preventDefault();
+        $('.overlay').show();
+    });
+});
